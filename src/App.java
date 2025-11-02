5b1953d5168702703f4be27363ac2db05a245c3d
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-11-02 11:44:38 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeroIntroducido = Integer.parseInt(System.console().readLine("Introduzca un número entero: "));
+        int digito = Integer.parseInt(System.console().readLine("Introduzca un digito: "));
+
+        long numero = numeroIntroducido;
+        numero = numero * 10 + 1;
+        long volteado = 0;
+        int longitud = 0;
+        int posicion = 1;
+
+        if (numero == 0) {
+            longitud = 1;
+        }
+
+        while (numero > 0) {
+            volteado = (volteado * 10) + (numero %10);
+            numero /= 10;
+            longitud++;
+        }
+
+        while (volteado != 1) {
+            if ((volteado % 10) == digito) {
+                System.out.print(posicion + " ");
+            }
+
+            volteado /= 10;
+            posicion++;
+        }
+        
+        System.out.println();
     }
 }
